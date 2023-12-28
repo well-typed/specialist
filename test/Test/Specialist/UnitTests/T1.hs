8e0b9c3a710035afb4508ad910526001a6ac641b
Finley McIlwaine <finley@well-typed.com>
2023-12-28 15:59:30 -0700

    Explicit superclass check in T1 unit test

--- test/Test/Specialist/UnitTests/T1.hs
+++ test/Test/Specialist/UnitTests/T1.hs
@@ -3,6 +3,7 @@ module Test.Specialist.UnitTests.T1 where
 import GHC.Specialist
 import Test.Specialist.Types
 
+import Data.List
 import System.Directory
 import System.Process
 import GHC.InfoProv
@@ -20,20 +21,36 @@ getNotesT1 :: IO [SpecialistNote]
 getNotesT1 = do
     callCommand "test-T1 +RTS -l-au -RTS"
     specialistNotesFromEventLogFile "test-T1.eventlog" >>= \case
-      Left  _ ->
-        error "failed to read specialist notes from test-T1.eventlog"
       Right notes -> do
         removeFile "test-T1.eventlog"
         return notes
+      Left  _ ->
+        assertFailure "failed to read specialist notes from test-T1.eventlog"
 
 testT1 :: [SpecialistNote] -> TestTree
 testT1 notes = testCase "T1" $ do
   length notes == 2 @?
-    "Expect exactly two overloaded calls"
+    "expect exactly two overloaded calls"
   any (includesDictLabel "$fShowX") notes @?
-    "Expect a call to include the Show X dictionary"
-  any (includesDictLabel "$fShowList") notes @?
-    "Expect a call to include the show list dictionary constructor for X"
+    "expect a call to include the Show X dictionary"
+  case find (includesDictLabel "$fShowList") notes of
+    Just SpecialistNote{..} -> do
+      case specialistNoteDictInfos of
+        [showListDict] ->
+          case dictInfoFreeDicts showListDict of
+            [showXDict] ->
+              case dictInfoProv showXDict of
+                Just InfoProv{..} ->
+                  "$fShowX" == ipLabel @?
+                    "expect the Show X dictionary to be the superclass referenced by the ShowList constructor"
+                Nothing ->
+                  assertFailure "expect to be able to find the info table provenance of the superclass dictionary"
+            _ ->
+              assertFailure "expect exactly one superclass referenced in the call including the Show List dictionary"
+        _ ->
+          assertFailure "expect one dictionary in the call that includes the ShowList dictionary constructor"
+    Nothing ->
+      assertFailure "expect a call to include the ShowList dictionary constructor"
 
 -- | Checks whether at least one of the dictionary infos in the note references
 -- a dictionary with the given label string.
