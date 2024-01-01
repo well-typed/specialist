5c84e131c64fb404b69a697a16da30350bc365f2
Finley McIlwaine <finley@well-typed.com>
2024-01-01 10:31:08 -0800

    Refactor superclass checks in test suite

--- test/Test/Specialist/Utils.hs
+++ test/Test/Specialist/Utils.hs
@@ -2,8 +2,11 @@ module Test.Specialist.Utils where
 
 import GHC.Specialist
 
+import Control.Monad
 import Data.List
 import GHC.InfoProv
+import Test.Tasty.HUnit
+
 
 -- | Find the first element of a list satisfying a predicate and return that
 -- element and the list with the element removed.
@@ -17,20 +20,75 @@ findAndRemove p (x:xs)
 -- a dictionary whose label string contains the given string.
 includesDictLabel :: String -> SpecialistNote -> Bool
 includesDictLabel s SpecialistNote{..} =
-  any (dictIncludesDictLabel s) specialistNoteDictInfos
+    any (dictIncludesDictLabel s) specialistNoteDictInfos
 
 -- | Checks whether at least one of the dictionary infos in the note references
 -- a dictionary whose name string contains the given string.
 includesDictName :: String -> SpecialistNote -> Bool
 includesDictName s SpecialistNote{..} =
-  any (dictIncludesDictName s) specialistNoteDictInfos
+    any (dictIncludesDictName s) specialistNoteDictInfos
 
 -- | Checks whether the IPE label of the 'DictInfo' contains the given string.
 dictIncludesDictLabel :: String -> DictInfo -> Bool
 dictIncludesDictLabel s =
-  maybe False ((s `isInfixOf`) . ipLabel) . dictInfoProv
+    maybe False ((s `isInfixOf`) . ipLabel) . dictInfoProv
 
 -- | Checks whether the IPE name of the 'DictInfo' contains the given string.
 dictIncludesDictName :: String -> DictInfo -> Bool
 dictIncludesDictName s =
-  maybe False ((s `isInfixOf`) . ipName) . dictInfoProv
+    maybe False ((s `isInfixOf`) . ipName) . dictInfoProv
+
+-- | Check that the dictionaries referenced in the note abide by some basic
+-- superclass relationships
+checkBasicSuperclasses :: SpecialistNote -> Assertion
+checkBasicSuperclasses SpecialistNote{..} =
+    mapM_ checkBasicDictSuperclasses specialistNoteDictInfos
+
+-- | Check that the dictionaries referenced in the note abide by some basic
+-- superclass relationships from the Prelude in addition to those given.
+checkSuperclasses :: [(String, String)] -> SpecialistNote -> Assertion
+checkSuperclasses rs SpecialistNote{..} =
+    mapM_ (checkDictSuperclasses rs) specialistNoteDictInfos
+
+-- | Check that the dictionaries referenced in the note abide by the given
+-- superclass relationships.
+checkSuperclassesWith :: [(String, String)] -> SpecialistNote -> Assertion
+checkSuperclassesWith rs SpecialistNote{..} =
+    mapM_ (checkDictSuperclassesWith rs) specialistNoteDictInfos
+
+-- | Check some basic superclass relationships for the given dictionary and it's
+-- superclasses
+checkBasicDictSuperclasses :: DictInfo -> Assertion
+checkBasicDictSuperclasses = checkDictSuperclasses []
+
+-- | Check that the given superclass relationships in addition to some basic
+-- Prelude class relationships hold for the given dictionary and it's
+-- superclasses.
+checkDictSuperclasses :: [(String, String)] -> DictInfo -> Assertion
+checkDictSuperclasses =
+    checkDictSuperclassesWith .
+      (
+        [ ("C:Ord", "C:Eq")
+        , ("C:Real", "C:Num")
+        , ("C:Real", "C:Ord")
+        , ("C:Integral", "C:Real")
+        , ("C:Integral", "C:Enum")
+        ] ++
+      )
+
+-- | Check that the given superclass relationships hold for the given
+-- dictionary.
+checkDictSuperclassesWith
+  :: [(String, String)]
+  -- ^ Assoc. list mapping classes to their expected superclasses
+  -> DictInfo
+  -- ^ 'DictInfo' to check
+  -> Assertion
+checkDictSuperclassesWith rs di@DictInfo{..} = do
+    mapM_ checkGivenRel rs
+    mapM_ (checkDictSuperclassesWith rs) dictInfoFreeDicts
+  where
+    checkGivenRel (c, sc) =
+      when (dictIncludesDictName c di) $
+        any (dictIncludesDictName sc) dictInfoFreeDicts @?
+          "expect " ++ sc ++ " to be a superclass of " ++ c
